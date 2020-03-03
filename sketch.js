45427d485bc0ec822ec63f866807e76e45e6e78a
adalek <61150101+adalek@users.noreply.github.com>
2020-03-03 12:13:16 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
+let slider1;
 
+function setup(){
+    createCanvas(windowWidth, windowHeight)
+    silder1 = createSlider(0.255);
+}
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);}
+function draw(){
+    background(silder1.value(),0,0)
+}
+
   
