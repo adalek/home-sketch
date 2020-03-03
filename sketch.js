c65ad9f55c8a81aeb2e5f674648d781d7ff1e323
adalek <61150101+adalek@users.noreply.github.com>
2020-03-03 12:22:32 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let slider1;
+/*let slider1;
 
 function setup(){
     createCanvas(windowWidth, windowHeight)
@@ -6,7 +6,20 @@ function setup(){
 }
 
 function draw(){
-    background(silder1.value(),0,0)
-}
+    background(100,silder1.value(),100)
+}*/
 
+let slider1;
+let slider2;
+let slider3;
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    slider1 = createSlider(0,255);
+    slider2 = createSlider(0,255);
+    slider3= createSlider(0,255);
+}
+function draw(){
+    background(slider1.value(), slider2.value(), slider3.value())
+}
   
